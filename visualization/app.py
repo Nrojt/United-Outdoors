e9c7003aebe82bf6454562fd1d958566701d66fc
Tjorn <74906283+Nrojt@users.noreply.github.com>
2024-05-06 12:24:53 +0200

    amazing website

--- visualization/app.py
+++ visualization/app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/images')
+def images():
+    image_names = os.listdir('static/images')
+    print(image_names)
+    return render_template("images.html", image_names=image_names)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5001, debug=True)
