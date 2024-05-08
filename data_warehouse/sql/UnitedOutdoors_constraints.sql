3e56c5a1f768947ca38dcfa3dda34f7533fe6da8
Tjorn <74906283+Nrojt@users.noreply.github.com>
2024-05-08 22:00:45 +0200

    sqlalchemy for database insertion

--- data_warehouse/sql/UnitedOutdoors_constraints.sql
+++ data_warehouse/sql/UnitedOutdoors_constraints.sql
@@ -1,2 +1,1 @@
--- foreign key constraints
 USE UnitedOutdoors;
--- data_warehouse/sql/UnitedOutdoors_creation.sql
+++ data_warehouse/sql/UnitedOutdoors_creation.sql
@@ -6,4 +6,27 @@ IF NOT EXISTS (
 CREATE DATABASE UnitedOutdoors
 GO
 
--- TODO: change everything below to the actual combined tables we'll be using
+USE UnitedOutdoors;
+
+CREATE TABLE NorthWindCategories (
+    "CategorySK" "int" PRIMARY KEY IDENTITY(1,1) NOT NULL ,
+    "CategoryID" "int" NOT NULL ,
+	"CategoryName" nvarchar (15) NOT NULL ,
+	"Description" "ntext" NULL ,
+	"Picture" "image" NULL
+);
+
+CREATE TABLE NorthWindCustomers (
+    "CustomerSK" "int" PRIMARY KEY IDENTITY(1,1) NOT NULL ,
+    "CustomerID" nchar (5) NOT NULL ,
+	"CompanyName" nvarchar (40) NOT NULL ,
+	"ContactName" nvarchar (30) NULL ,
+	"ContactTitle" nvarchar (30) NULL ,
+	"Address" nvarchar (60) NULL ,
+	"City" nvarchar (15) NULL ,
+	"Region" nvarchar (15) NULL ,
+	"PostalCode" nvarchar (10) NULL ,
+	"Country" nvarchar (15) NULL ,
+	"Phone" nvarchar (24) NULL ,
+	"Fax" nvarchar (24) NULL
+);
