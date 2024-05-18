71295b1b1989d218194e6adc5dfdad4d91765647
Tjorn <74906283+Nrojt@users.noreply.github.com>
2024-05-18 16:15:08 +0200

    Fixed the renaming

--- data_warehouse/source_data/sql/Aenc_foreign_keys.sql
+++ data_warehouse/source_data/sql/Aenc_foreign_keys.sql
@@ -2,12 +2,12 @@ USE Aenc;
 
 -- Adding foreign keys
 ALTER TABLE "Bonus" ADD FOREIGN KEY ("emp_id") REFERENCES "Employee" ("emp_id");
-ALTER TABLE "department" ADD FOREIGN KEY ("dept_head_id") REFERENCES "Employee" ("emp_id");
-ALTER TABLE "Employee" ADD FOREIGN KEY ("dept_id") REFERENCES "department" ("dept_id");
+ALTER TABLE "Department" ADD FOREIGN KEY ("dept_head_id") REFERENCES "Employee" ("emp_id");
+ALTER TABLE "Employee" ADD FOREIGN KEY ("dept_id") REFERENCES "Department" ("dept_id");
 ALTER TABLE "Employee" ADD FOREIGN KEY ("manager_id") REFERENCES "Employee" ("emp_id");
-ALTER TABLE "sales_order" ADD FOREIGN KEY ("cust_id") REFERENCES "customer" ("id");
-ALTER TABLE "sales_order" ADD FOREIGN KEY ("region") REFERENCES "region" ("region_name");
-ALTER TABLE "sales_order" ADD FOREIGN KEY ("sales_rep") REFERENCES "Employee" ("emp_id");
-ALTER TABLE "sales_order_item" ADD FOREIGN KEY ("prod_id") REFERENCES "product" ("id");
-ALTER TABLE "state" ADD FOREIGN KEY ("region") REFERENCES "region" ("region_name");
+ALTER TABLE "SalesOrder" ADD FOREIGN KEY ("cust_id") REFERENCES "customer" ("id");
+ALTER TABLE "SalesOrder" ADD FOREIGN KEY ("region") REFERENCES "Region" ("region_name");
+ALTER TABLE "SalesOrder" ADD FOREIGN KEY ("sales_rep") REFERENCES "Employee" ("emp_id");
+ALTER TABLE "SalesOrderItem" ADD FOREIGN KEY ("prod_id") REFERENCES "Product" ("id");
+ALTER TABLE "State" ADD FOREIGN KEY ("region") REFERENCES "Region" ("region_name");
 GO
