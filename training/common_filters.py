cfe561c846f0d59a4b3060e2c53e7180b0a3e0a5
Tjorn <74906283+Nrojt@users.noreply.github.com>
2024-05-21 13:03:24 +0200

    Added dataframe to visualize pytorch training Moved train_model to common_functions.py (renamed common_filters.py)

--- training/common_filters.py
+++
@@ -1,6 +0,0 @@
-def drop_sk_datetime_added_columns(dataframe):
-    # dropping all columns with 'rowguid' in their name
-    columns_to_drop_mr = dataframe.filter(like='_sk').columns.append(dataframe.filter(like='_datetime_added').columns)
-
-    # dropping the columns
-    dataframe.drop(columns=columns_to_drop_mr, inplace=True)
